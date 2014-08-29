8e9db8981ffe6a2460d3ab028cc8b8c94648bfe7
mikowals <mikowals@gmail.com>
2014-08-29 14:15:54 +1000

    format readme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "name": "mikowals:find-async",
-  "version": "0.0.5",
+  "version": "0.0.6",
   "git": "https://github.com/mikowals/find-async.git",
   "summary": "Extend Meteor.Collection with asynchronous mongo functions and aggregation."
 });
