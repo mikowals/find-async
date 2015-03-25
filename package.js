b56c7db16fc46c63d00748d29f746437a2b8f00e
mikowals <mikowals@gmail.com>
2015-03-25 15:55:47 +1000

    readme

--- package.js
+++ package.js
@@ -1,20 +1,18 @@
 Package.describe({
   "name": "mikowals:find-async",
-  "version": "0.0.8",
+  "version": "1.0.0",
   "git": "https://github.com/mikowals/find-async.git",
   "summary": "Extend Meteor.Collection with asynchronous mongo functions and aggregation."
 });
-Npm.depends({
-//  "fibers":"1.0.2"
-})
+
 Package.onUse( function( api ){
-  api.versionsFrom( 'METEOR@0.9.1' );
-  api.use( ['mquandalle:harmony@1.2.1', 'mongo', 'meteor'], 'server');
-  api.addFiles( 'findAsync.next.js', 'server');
+  api.versionsFrom( 'METEOR@1.0.3' );
+  api.use( ['grigio:babel@0.0.12', 'mongo'], 'server');
+  api.addFiles( 'findAsync.es6.js', 'server');
   api.export( 'Future', 'server');
 });
 
 Package.onTest( function( api ){
-  api.use( ['mikowals:harmony', 'mikowals:find-async','mongo', 'tinytest', 'test-helpers'], 'server');
-  api.addFiles('findAsync-test.next.js', 'server');
+  api.use( ['grigio:babel', 'mongo','mikowals:find-async', 'tinytest', 'test-helpers'], 'server');
+  api.addFiles('findAsync-test.es6.js', 'server');
 });
